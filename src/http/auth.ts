353ba3efcc679f819b24911f54db9a65d471f2d6
Ian <ssianfilipe@gmail.com>
2024-04-29 23:14:23 -0300

    feat: initial setup and auth routes

---
+++ src/http/auth.ts
@@ -0,0 +1,57 @@
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
+import Elysia, { t, type Static } from 'elysia'
+import { UnauthorizedError } from './errors/unauthorized-error'
+
+const jwtPayload = t.Object({
+  sub: t.String(),
+  restaurantId: t.Optional(t.String()),
+})
+
+export const auth = new Elysia()
+  .error({
+    UNAUTHORIZED: UnauthorizedError,
+  })
+  .onError(({ error, code, set }) => {
+    switch (code) {
+      case 'UNAUTHORIZED': {
+        set.status = 401
+        return { code, message: error.message }
+      }
+    }
+  })
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: jwtPayload,
+    }),
+  )
+  .derive({ as: 'scoped' }, ({ jwt, cookie: { auth } }) => {
+    return {
+      signUser: async (payload: Static<typeof jwtPayload>) => {
+        const token = await jwt.sign(payload)
+
+        auth.value = token
+        auth.httpOnly = true
+        auth.maxAge = 60 * 60 * 24 * 7 // 7 days
+        auth.path = '/'
+      },
+
+      signOut: async () => {
+        auth.remove()
+      },
+
+      getCurrentUser: async () => {
+        const payload = await jwt.verify(auth.value)
+
+        if (!payload) {
+          throw new UnauthorizedError()
+        }
+
+        return {
+          userId: payload.sub,
+          restaurantId: payload.restaurantId,
+        }
+      },
+    }
+  })
---
+++ src/http/routes/get-managed-restaurant.ts
@@ -0,0 +1,21 @@
+import Elysia from 'elysia'
+import { auth } from '../auth'
+import { db } from '../../db/connection'
+
+export const getManagedRestaurant = new Elysia()
+  .use(auth)
+  .get('/managed-restaurant', async ({ getCurrentUser }) => {
+    const { restaurantId } = await getCurrentUser()
+
+    if (!restaurantId) {
+      throw new Error('User is not a manager.')
+    }
+
+    const managedRestaurant = await db.query.restaurants.findFirst({
+      where({ id }, { eq }) {
+        return eq(id, restaurantId)
+      },
+    })
+
+    return managedRestaurant
+  })
